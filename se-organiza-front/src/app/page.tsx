1e215d2c7e68cbcf6b251df9602aa8eb57f6261e
Pedro Rios <PRios731@gmail.com>
2024-07-04 16:19:07 -0300

    login page

--- se-organiza-front/src/app/page.tsx
+++ se-organiza-front/src/app/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { CircleDollarSign } from 'lucide-react'; 
 
 export default function Home() {
   return (
     <main>
-      <h1>Se Organiza</h1>
-      
+      <div className="flex pb-60 items-center h-screen">
+
+        <div className="container space-y-4 p-8 max-w-md rounded-xl bg-gray-50 shadow-xl">
+          <span className="flex items-center gap-2">
+            <CircleDollarSign className=" text-orange-400" size={48} />
+            <h1 className=" uppercase text-zinc-600 font-bold text-2xl">Se Organiza</h1>
+          </span>
+          <Input type="email" placeholder="Digite seu email..."/>
+          <Input type="password" placeholder="Digite sua senha..."/>
+          <div className="flex flex-col items-center justify-center">
+            <Button className="">Entrar</Button>
+          </div>
+          
+        </div>
+      </div>
     </main>
   );
 }
