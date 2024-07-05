ffbfc8e721a84cd1849a1ec421044cae71d99383
Pedro Rios <PRios731@gmail.com>
2024-07-05 15:53:40 -0300

    sign up page

---
+++ se-organiza-front/src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import { CircleDollarSign } from "lucide-react";
+
+export default function DashboardLayout({ children } : { children : React.ReactNode}){
+    return(
+        <>
+            <div className="container flex h-20 rounded-xl items-center m-auto gap-4 shadow-md bg-zinc-800">
+                <CircleDollarSign  className="text-orange-500" size={48} />  
+                <h1 className=" uppercase text-zinc-100 font-bold text-3xl">Se Organiza</h1>
+            </div>
+            <div className="container items-center gap-4 w-md m-auto mt-2 rounded-xl shadow-md h-40 bg-zinc-50">
+                {children}
+            </div>
+        </>
+    );
+}
---
+++ se-organiza-front/src/components/dashboard/insert-activity-form/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+export function InsertActivityForm(){
+    return(
+        <div className="flex space-x-2 p-6">
+            <Input type="date" className="max-w-[10rem]" />
+            <Input type="text" placeholder="Insira a descrição da atividade..."/>
+            <Input type="number" className="w-max" placeholder="Digite o valor"/>
+            <Select>
+                <SelectTrigger className="w-80">
+                    <SelectValue placeholder="Selecione o fluxo de caixa" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="0">Entrada</SelectItem>
+                    <SelectItem value="1">Saída</SelectItem>
+                </SelectContent>
+            </Select>
+            <Select>
+                <SelectTrigger className="w-80">
+                    <SelectValue placeholder="Selecione o tipo" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="0">Educação</SelectItem>
+                    <SelectItem value="1">Entretenimento</SelectItem>
+                    <SelectItem value="3">Alimentação</SelectItem>
+                </SelectContent>
+            </Select>
+            <Button>Incluir</Button>
+        </div>
+    );
+}
